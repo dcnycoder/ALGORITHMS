73cd0feb0caa53e5fe3bc2b89daf8d8aaf71cbbf
dcnycoder <dc5stars@gmail.com>
2019-08-11 14:47:58 -0400

    permutations solved

--- Wayfair/permutations.js
+++ Wayfair/permutations.js
@@ -1,22 +1,27 @@
 const array = [1,2,3];
 
-function permutations(array, result = []) {
+function permutations(array, restArray = [], resultArray = []) {
   //let result = [];
   //if (array.length === 0) return [];
   //console.log('result: ', result);
-  if (array.length === 1) return array[0];
+  //let result = [];
+  if (array.length === 0) {
+    resultArray.push(restArray);
+    //return resultArray;
+  }
   else
 
   for (let i = 0; i<array.length; i++) {
-    result = [];
+    //result = [];\
     //result[i].push(array[i]);
-    result[i] = array[i];
     //console.log('result: ', result[0]);
-    result.concat(permutations(array.slice(0, i).concat(array.slice(i+1, array.length)), result));
+    //result.concat(permutations(array.slice(0, i).concat(array.slice(i+1, array.length)), result));
+    //resultArray.push(
+      permutations(array.slice(0, i).concat(array.slice(i+1, array.length)), restArray.concat(array[i]), resultArray);
     //result.push(permutations(array.slice(0, i).concat(array.slice(i+1, array.length))));
     // result.push(permutations(array.slice(0, i).concat(array.slice(i+1, array.length))));
   }
-  return result;
+  return resultArray;
 }
 
 console.log(permutations(array));
---
+++ Wayfair/permutationsSolution.js
@@ -0,0 +1,14 @@
+function p(a, b, res) {
+  var b = b || [], res = res || [], len = a.length;
+  if (!len)
+    res.push(b)
+  else
+    for (var i = 0; i < len; i++)
+         // recursive call to `p` here
+    {
+      p(a.slice(0, i).concat(a.slice(i + 1, len)), b.concat(a[i]), res);
+    }
+  return res;
+}
+
+console.log(p(["a", "b", "c"]));
