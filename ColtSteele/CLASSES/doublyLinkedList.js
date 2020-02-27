fa97d46ac46dc7f2351c03356094b24933309d93
dcnycoder <dc5stars@gmail.com>
2020-02-27 17:53:51 -0500

    Doubly Linked List done

---
+++ ColtSteele/CLASSES/doublyLinkedList.js
@@ -0,0 +1,17 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class doublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
+
+
