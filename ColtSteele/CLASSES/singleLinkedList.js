e8b710b418ac83536ec5d2d45a8ec74a87a1d16d
dcnycoder <dc5stars@gmail.com>
2020-02-27 15:41:58 -0500

    Singly Linked List reverse() done

--- ColtSteele/CLASSES/singleLinkedList.js
+++ ColtSteele/CLASSES/singleLinkedList.js
@@ -6,14 +6,15 @@ class Node {
 }
 
 class LinkedList {
-  constructor(value) {
-    this.head = new Node(value);
+  constructor() {
+    this.head = null;
     this.tail = this.head;
-    this.length = 1;
+    this.length = 0;
   }
   addNode(value) {
     if (!this.head) {
-      this.head = new Node(value);
+      this.head = this.tail = new Node(value);
+      this.length += 1;
     }
     else {
       this.tail.next = new Node(value);
@@ -38,6 +39,7 @@ class LinkedList {
       result = current.next;
       this.tail = current;
       current.next = null;
+      this.length -= 1;
       return result;
     }
   }
@@ -69,7 +71,7 @@ class LinkedList {
     return this;
   }
   get(index) {
-    if (index>this.length) {
+    if (index>this.length || index<0) {
       console.log("The index wasn't found!");
       return undefined;
     }
@@ -99,27 +101,52 @@ class LinkedList {
     }
     return this;
   }
+  remove(index) {
+    if (index >= this.length || index < 0) return;
+    else if (index === 0) {
+      this.shift();
+    }
+    else if (index === this.length-1) {
+      this.popNode()
+    }
+    else {
+      let prevNode = this.get(index-1);
+      let nextNode = this.get(index+1);
+      prevNode.next =  nextNode;
+      this.length -= 1;
+    }
+    return this;
+  }
+  reverse() {
+    let prevElem = null;
+    let nextElem;
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+
+    for (let i=0; i<this.length; i++) {
+      //we start working with the current node, that is the former HEAD;
+      nextElem = node.next; //to save ref to next elem after HEAD before we change it
+      node.next = prevElem; //we set the current node next to the previous elem, which is the new tail (OLD HEAD) in the beginning of this loop;
+      prevElem = node; // we change the previous element to be this node for future reference;
+      node = nextElem; // we switch to the next node to repeat the cycle again
+    }
+  return this;
+  }
+  printValues() {
+    let values = [];
+    let elem = this.head;
+    while (elem) {
+      values.push(elem.value);
+      elem = elem.next;
+    }
+    return values;
+  }
 }
 
-let list = new LinkedList(1);
-list.addNode(5);
-list.addNode(10);
-// list.addNode(5);
-// list.addNode(1);
-// list.addNode(0)
-
-console.log('list: ', list);
-console.log('list after insertion: ', list.insert(3, 2));
-// console.log("list after unshift(100)", list.unshift(100));
-// console.log("list after unshift(200)", list.unshift(200));
-// console.log('list.get(0): ', list.get(1));
-// console.log('list.set(0, 500): ', list.set(1, 500));
-// console.log('list after 0 set to 500: ', list)
-// console.log('list.shift(): ', list.shift());
-// console.log('list.shift(): ', list.shift());
-// console.log('list after shift(): ', list);
-// list.popNode();
-// list.popNode();
-//console.log('pop result: ', list.popNode());
-//console.log('list after pop(): ', list);
+let list = new LinkedList().addNode(1).addNode(5).addNode(10);
+// console.log('pop 10: ', list.popNode());
+console.log('list: ', list.printValues());
+let reversedList = list.reverse();
+console.log('Reversed list: ', reversedList.printValues());
 
