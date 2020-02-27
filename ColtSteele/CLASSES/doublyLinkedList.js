2d4d157a9cdeae8b08a247de38cf365cdf8da998
dcnycoder <dc5stars@gmail.com>
2020-02-27 17:54:49 -0500

    Doubly Linked List push() done

--- ColtSteele/CLASSES/doublyLinkedList.js
+++ ColtSteele/CLASSES/doublyLinkedList.js
@@ -12,6 +12,19 @@ class doublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(value) {
+    let node = new Node(value);
+    if (!this.head) {
+      this.head = this.tail = node;
+    }
+    else {
+      node.prev = this.tail;
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length += 1;
+    return this;
+  }
 }
 
 
