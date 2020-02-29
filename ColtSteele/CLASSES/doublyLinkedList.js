d421235612a803d9014dafe234261f6d13225b36
dcnycoder <dc5stars@gmail.com>
2020-02-29 10:47:57 -0500

    Doubly Linked List reverse() done

--- ColtSteele/CLASSES/doublyLinkedList.js
+++ ColtSteele/CLASSES/doublyLinkedList.js
@@ -45,6 +45,26 @@ class doublyLinkedList {
     return result;
   }
 
+  reverse() {
+    //Make current head a tail:
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let prevElem = null;
+    let nextElem;
+    for (let i=0; i<this.length; i++) {
+      //we save the reference to nextElem beause we'll be reassigning it to a previous element to reverse the direction of the list later
+      nextElem = node.next;
+      //we change the direction by assigning the node.next to the prevElement and node.prev to nextElem.
+      node.next = prevElem;
+      node.prev = nextElem;
+      //then we move the 'sliding window': we assign new node to prevElem, and the node becomes nextElem and we repeat the cycle.
+      prevElem = node;
+      node = nextElem;
+    }
+    return this;
+  }
+
   printValues() {
     let values = [];
     let elem = this.head;
@@ -58,9 +78,10 @@ class doublyLinkedList {
 
 let list = new doublyLinkedList().push(1).push(2).push(3).push(4);
 
-console.log("List: ", list);
-console.log('pop result: ', list.pop());
-console.log('list after pop: ', list)
+console.log("List: ", list.printValues());
+// console.log('pop result: ', list.pop());
+// console.log('list after pop: ', list.printValues())
 //console.log("This.head.next: ", list.head.next.prev);
+console.log("this list reversed: ", list.reverse().printValues());
 
 
