81ea89629b9e271e77cba0937d0e442e229709e4
dcnycoder <dc5stars@gmail.com>
2020-03-18 21:04:52 -0400

    Hash Table set() done

--- ColtSteele/DATA_STRUCTURES/hashTable.js
+++ ColtSteele/DATA_STRUCTURES/hashTable.js
@@ -7,13 +7,34 @@ class HashTable {
     const primeNumber = 31
     let total = 0
     for (let i = 0; i<Math.min(key.length || 100); i++) {
-      let value = key[i].charCodeAt()-96
-      console.log(value)
+      let value = key[i].charCodeAt()
       total = (total*primeNumber+value)%this.keyMap.length
     }
     return total
   }
+
+  set(value) {
+    console.log('value before hashing: ', value)
+    const index = this.hash(value[0])
+    if (!this.keyMap[index]) this.keyMap[index] = [value]
+    else {
+      let valuePresent = this.keyMap[index].reduce((acc, elem) =>
+        {
+          return acc || (value[0] === elem[0])? true : false
+        }
+      , false)
+      console.log('valuePresent: ', valuePresent)
+      if (!valuePresent) this.keyMap[index].push(value)
+    }
+    console.log(this.keyMap[index], index)
+  }
+
+  get(value) {
+
+  }
 }
 
 let ht = new HashTable
-console.log('ht hash hello: ', ht.hash('hello'))
+console.log('ht hash hello: ', ht.set(['blue', '#394043']))
+console.log('ht hash hello: ', ht.set(['blue', '#394043']))
+
