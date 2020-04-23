68e0464a69359624504b8c248fbdba74a2fc5f56
dcnycoder <dc5stars@gmail.com>
2020-04-22 20:07:18 -0400

    graph addEdge done

--- ColtSteele/DATA_STRUCTURES/graphs.js
+++ ColtSteele/DATA_STRUCTURES/graphs.js
@@ -6,7 +6,13 @@ class Graph {
     if (!(vertex in this.adjacencyList)) this.adjacencyList[vertex] = []
     return this
   }
-
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      if (!(vertex2 in this.adjacencyList[vertex1])) this.adjacencyList[vertex1].push(vertex2)
+      if (!(vertex1 in this.adjacencyList[vertex2])) this.adjacencyList[vertex2].push(vertex1)
+      return this
+    }
+  }
 }
 
 let graph = new Graph
