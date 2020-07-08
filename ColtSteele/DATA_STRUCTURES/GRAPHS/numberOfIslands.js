e7b7ddf27974c3800ed00e6cff588a0383470f0e
dcnycoder <dc5stars@gmail.com>
2020-07-08 08:24:27 -0400

    numberOfIslands: works but have to figure out how to automatically fill visited array

--- ColtSteele/DATA_STRUCTURES/GRAPHS/numberOfIslands.js
+++ ColtSteele/DATA_STRUCTURES/GRAPHS/numberOfIslands.js
@@ -9,34 +9,49 @@
 const map = [
   [1,1,1,1,0],
   [1,1,0,1,0],
-  [1,1,0,0,0],
+  [1,0,1,0,0],
   [0,0,1,0,1]
+]
+
+const map1 = [
+  [1,0,1],
+  [0,1,0],
+  [1,0,1]
 ]
 
 function numberOfIslands(map) {
   let islands = 0
-  let visited = new Array(map.length).fill(new Array(map[0].length).fill(null))
-  function exploreRecursive(vertex) {
-    let neighbors = []
-    if ()
+  // let visited = new Array(map.length).fill(new Array(map[0].length).fill(null))
+  let visited= [
+    [null,null,null],
+    [null,null,null],
+    [null,null,null]
+  ]
+  function exploreRecursive(row, column) {
+    if (column+1 < map[row].length && map[row][column+1] === 1) exploreRecursive(row, column+1)
+    if (row+1 < map.length && map[row+1][column]  === 1) exploreRecursive(row+1, column)
+    visited[row][column] = true
   }
   for (let row = 0; row<map.length; row++) {
-    for (let column = 0; column<row.length; i++) {
-      let vertex = map[row][column]
-      if (!visited.includes(vertex)) {
-        islands+=1
-        exploreRecursive(vertex)
+    for (let column = 0; column<map[row].length; column++) {
+      //let vertex = map[row][column]
+      if (visited[row][column] === null) {
+        if (map[row][column] === 1) {
+          console.log("Row/column", row, column)
+          islands+=1
+          exploreRecursive(row, column)
+        }
       }
       visited[row][column] = true
     }
   }
+  return islands
 }
 
-console.log(numberOfIslands(map))
+console.log(numberOfIslands(map1))
 
-let testArray = new Array(4)
-testArray.fill(4)
-//console.log(testArray)
+// let testArray = new Array(4)
+// testArray.fill(4)
 
 
 
