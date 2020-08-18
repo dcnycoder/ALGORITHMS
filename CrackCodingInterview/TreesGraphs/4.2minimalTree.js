98bcde49b0bb617c8d972dbb421d336f1f10b492
dcnycoder <dc5stars@gmail.com>
2020-08-18 15:13:14 -0400

    minHeightBST refactored to work with Leetcode structure

--- CrackCodingInterview/TreesGraphs/4.2minimalTree.js
+++ CrackCodingInterview/TreesGraphs/4.2minimalTree.js
@@ -1,19 +1,23 @@
 // p.109 problem 4.2 Minimal Tree: given a sorted(increasing order) array with unique integer elements, write an algorithm to create a binary search tree with minimal height
 
-class Node {
-  constructor(value) {
-    this.value = value
-    this.left = null
-    this.right = null
+// * function TreeNode(val, left, right) {
+//   *     this.val = (val===undefined ? 0 : val)
+//   *     this.left = (left===undefined ? null : left)
+//   *     this.right = (right===undefined ? null : right)
+//   * }
+
+
+class Node { // class-based
+  constructor(value, left, right) {
+    this.value = (value? value : 0)
+    this.left = (left? left : null)
+    this.right = (right? right : null)
   }
 }
 
-class BST {
+class BST { // separate class for BST
   constructor(node) {
-    if (!node) {
-      this.root = new Node(null)
-    }
-    else this.root = node
+    this.root = new Node(node)
   }
   addNode(value) {
     let node
@@ -45,19 +49,61 @@ class BST {
       } // end of if new node value < less node value clause
       else return this
     }
-
-    // function insertNode(node, newNode) {
-
-    // } //end of insertNode
-
-    // else return insertNode(this.root, node)
   } // end of addNode
 } // end of class BST
 
-let bst = new BST
-console.log("BST after addNodes: ", bst
-  .addNode(50)
-  .addNode(100)
-  .addNode(25)
-  .addNode(75)
-)
+function TreeNode(val, left, right) {
+   this.val = (val===undefined ? 0 : val)
+   this.left = (left===undefined ? null : left)
+   this.right = (right===undefined ? null : right)
+}
+
+var minHeightBST = function(array) { // without separate BST class
+  function addNode(root, newNode) {
+    node = root
+    newNode = new TreeNode(newNode)
+    while (true) {
+      if (newNode.val<node.val) {
+        if (!node.left) {
+          node.left = newNode
+          return root
+        }
+        else {
+          node = node.left
+        }
+      } // end of if new node value < less node value clause
+      else if (newNode.val>node.val) {
+        if (!node.right) {
+          node.right = newNode
+          return root
+        }
+        else {
+          node = node.right
+        }
+        //return insertNode(node.right, node)
+      } // end of if new node value < less node value clause
+      else return root
+    }
+  }
+  const middle = Math.min(array.length/2)
+  let bst = new TreeNode(array[middle])
+
+  array.splice(middle, 1)
+  array.forEach(elem => {
+    addNode(bst, elem)
+  })
+  return bst
+} //end of minHeightBST
+
+const array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+
+console.log(minHeightBST(array))
+
+
+// let bst = new BST
+// console.log("BST after addNodes: ", bst
+//   .addNode(50)
+//   .addNode(100)
+//   .addNode(25)
+//   .addNode(75)
+// )
