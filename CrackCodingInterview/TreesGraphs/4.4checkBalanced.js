9b2490c8f7922ee2c5ba49111cea797c66d9b18f
dcnycoder <dc5stars@gmail.com>
2020-08-26 12:14:04 -0400

    checkBalanced: before rebuilding BTBuilder

--- CrackCodingInterview/TreesGraphs/4.4checkBalanced.js
+++ CrackCodingInterview/TreesGraphs/4.4checkBalanced.js
@@ -41,63 +41,71 @@
  * @return {boolean}
  */
 
+ //[5,2,2,3,3,3,3,4,4,4,4,4,4,null,null,5,5]
 function BTBuilder(array) {
   let root = new Node(array[0])
   let queue = [root]
   for (let i=1; i<array.length; i++) {
-    let currentNode = queue.pop()
-    if (array[i+1] != null) {
-      currentNode.right = new Node(array[i+1])
-      queue.push(currentNode.right)
-    }
-    else currentNode.right = null
-
-    if (array[i] != null) {
-      currentNode.left = new Node(array[i])
+    let currentNode = queue.unshift() //was pop()
+    if (array[2*i+1] != null) {
+      currentNode.left = new Node(array[2*i+1])
       queue.push(currentNode.left)
     }
     else currentNode.left = null
+
+    //was: if (array[i] != null) {
+    if (array[2*i+2] != null) {
+      currentNode.right = new Node(array[i])
+      queue.push(currentNode.right)
+    }
+    else currentNode.right = null
     i+=1
   }
   return root
 }
 
 var isBalanced = function(root) {
-  if (!root) return true
+  let result = true
+  if (!root) return result
   else {
 //The function below runs a BFS, finds and returns the shallowest and deepest branch heights
   function minMaxDifference(root) {
     let queue = [root]
     let level = 0
-    let min
-    let max
+    let min = undefined
+    let max = 0
     while (queue.length) {
       let length = queue.length
       level += 1
       for (let i=0; i<length; i++) {
         let node = queue.pop()
         if (node.left===null || node.right===null) {
-          if (!min) min = level
+          if (min===undefined) min = level
           else if (level>max) {
             max = level
-            if (max-min>1) return false
+            if ((max-min)>1) result = false
           }
         }
         if (node.left!=null) queue.push(node.left)
         if (node.right!=null) queue.push(node.right)
       }
     } // end of bst walk
-    return true
+    console.log("min: ", min)
+    console.log('max: ', max)
+    return result
   } //end of minMaxDifference
 
-  minMaxDifference(root)
+  return minMaxDifference(root)
 
   }
 }
 
 //console.log("Tree: ", BTBuilder([1,2,2,3,3,null,null,4,4]))
 //const bt = BTBuilder([1,2,2,3,3,null,null,4,4])
-const bt = BTBuilder([3,9,20,null,null,15,7])
+//const bt = BTBuilder([3,9,20,null,null,15,7])
+//const bt = BTBuilder([1,2,2,3,3,null,null,4,4])
+const bt = BTBuilder([5,2,2,3,3,3,3,4,4,4,4,4,4,null,null,5,5])
+
 console.log('bt: ', bt)
 console.log(isBalanced(bt))
 
