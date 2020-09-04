8395c21dc060a0abb28dee2707bc67fb1beab8eb
dcnycoder <dc5stars@gmail.com>
2020-09-04 18:21:48 -0400

    4.10checkSubtree: started

---
+++ CrackCodingInterview/TreesGraphs/4.10checkSubtree.js
@@ -0,0 +1,19 @@
+// T1 and T2 are two very large binary trees with T1 much bigger than T2. Create an algorithm to determine if T2 is a subtree of T1.
+// A tree T2 is a subtree of T1 if there exists a node in T1 such that the subtree of n is identical to T2. That is, if you cut off the tree at node n, the two trees would be identical.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} s
+ * @param {TreeNode} t
+ * @return {boolean}
+ */
+var isSubtree = function(s, t) {
+
+};
