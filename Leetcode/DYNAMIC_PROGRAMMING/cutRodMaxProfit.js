70fbd0edc1fcca9175f65f2506833aa4e008d010
dcnycoder <dc5stars@gmail.com>
2020-09-29 17:23:10 -0400

    cutRodMaxProfit: done

--- Leetcode/DYNAMIC_PROGRAMMING/cutRodMaxProfit.js
+++ Leetcode/DYNAMIC_PROGRAMMING/cutRodMaxProfit.js
@@ -1,15 +1,30 @@
 const prices = {
-  1: 2,
+  1: 1,
   2: 5,
-  5: 7,
-  10: 12,
+  3: 8,
+  4: 8.7,
+  5: 10,
+  6: 17,
+  7: 17,
+  8: 20,
+  9: 24,
+  10: 30
 }
-const rod = 10
+const rod = 8
 
 function maxProfit(length, prices) {
+  let q = -Infinity
   if (length === 0) return 0
-  else return Math.max(prices[length], 1+maxProfit(length-1, prices))
+  else
+    // return prices[length]? Math.max(prices[length], 1+maxProfit(length-1, prices)) : 1+maxProfit(length-1, prices)
+  for (let i=1; i<=length; i++) {
+    let maxProfitNext = maxProfit(length-i, prices)
+    q = Math.max(q, prices[length], prices[i]+maxProfitNext)
+  }
+  return q
 }
 
 console.log(maxProfit(rod, prices))
-//
+
+console.log(4>undefined)
+console.log(Math.max(undefined, 4))
