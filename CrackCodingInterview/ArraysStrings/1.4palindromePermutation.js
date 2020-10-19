9bbd1cfc9b3c2052679bbe57e8a9ee773872a6cc
dcnycoder <dc5stars@gmail.com>
2020-10-18 21:48:01 -0400

    1.4palindromePermutation: started

---
+++ CrackCodingInterview/ArraysStrings/1.4palindromePermutation.js
@@ -0,0 +1,13 @@
+const string = 'abba'
+
+/**
+ * @param s: the given string
+ * @return: if a permutation of the string could form a palindrome
+ */
+const canPermutePalindrome = function (s) {
+
+}
+
+console.log(canPermutePalindrome(string))
+
+
