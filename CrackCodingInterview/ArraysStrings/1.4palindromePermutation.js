787b02072b95e29f4a1ed6b93f98c41079da6572
dcnycoder <dc5stars@gmail.com>
2020-10-19 22:15:52 -0400

    1.4palindromePermutation: finished

--- CrackCodingInterview/ArraysStrings/1.4palindromePermutation.js
+++ CrackCodingInterview/ArraysStrings/1.4palindromePermutation.js
@@ -1,11 +1,34 @@
-const string = 'abba'
+//https://www.lintcode.com/problem/palindrome-permutation/description
 
 /**
  * @param s: the given string
  * @return: if a permutation of the string could form a palindrome
  */
-const canPermutePalindrome = function (s) {
 
+//Solution approach:
+//If the length of the string (spaces omitted) is odd, one odd element count is permitted. If not, only even count is possible.
+const string = 'ab    tttbba'
+
+const canPermutePalindrome = function (s) {
+  let result
+  let letterCount = {}
+  let totalLetters = 0
+  for (let i=0; i<s.length; i++) {
+    if (s[i] != ' ') {
+      totalLetters += 1
+      if (!(s[i] in letterCount)) letterCount[s[i]] = 1
+      else letterCount[s[i]]+=1
+    }
+  }
+  let countOdd = 0
+  for (let letter in letterCount) {
+    if (letterCount[letter]%2===1) countOdd+=1
+    if (countOdd>1) {
+      return false
+    }
+  }
+  if ((countOdd && totalLetters%2===1) || (!countOdd && !(totalLetters%2===1))) return true
+  else return false
 }
 
 console.log(canPermutePalindrome(string))
