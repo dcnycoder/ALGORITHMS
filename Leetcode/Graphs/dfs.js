e9b2e2265c45e0fb674ed0382122eada34ec3d36
dcnycoder <dc5stars@gmail.com>
2020-11-04 18:04:54 -0500

    Graps: DFS done

---
+++ Leetcode/Graphs/dfs.js
@@ -0,0 +1,35 @@
+// const graph = {
+//   "A": ["B", "C", "D"],
+//   "B": ["D", "E"],
+//   "C": [],
+//   "D": [],
+//   "E": []
+
+//   // "D": ["F"],
+//   // "E": [],
+//   // "F": []
+// }
+
+//SOLUTION APPROACH: 
+//1) Push 1st node into stack
+//2) Pop the node from stack, adding its neighbors to stack
+//2) Repeat until the stack is exhausted.
+
+const graph = {
+  "A": ["B", "C", "D"],
+  "B": ["D", "E"],
+  "C": [],
+  "D": ["F"],
+  "E": [],
+  "F": []
+}
+function dfs(graph, node, visited = {}, result = []) {
+  if (!(node in visited)) result.push(node)
+  visited[node] = true
+    graph[node].forEach((neighbor) => {
+      result = dfs(graph, neighbor, visited, result)
+    })
+  return result
+}
+
+console.log("DFS: ", dfs(graph, "A"))
