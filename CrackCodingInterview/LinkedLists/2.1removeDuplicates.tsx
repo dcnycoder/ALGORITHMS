512bd0d519b1229bf863e6a0878656a88ccc1795
dcnycoder <dc5stars@gmail.com>
2020-11-27 22:38:14 -0500

    2.1removeDuplicates: started

---
+++ CrackCodingInterview/LinkedLists/2.1removeDuplicates.tsx
@@ -0,0 +1,13 @@
+class LinkedList {
+  constructor(array) {
+    array.forEach(elem => {
+
+    })
+  }
+}
+
+function removeDuplicates(linkedList) {
+  
+}
+
+const LL = new LinkedList([1,2,3,4,5,6])
