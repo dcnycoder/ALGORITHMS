916f042fd6bd3fd34864de0f37e8c903f93bab83
dcnycoder <dc5stars@gmail.com>
2020-12-18 19:31:10 -0500

    2.4partitionLinkedList.js: LLNode class done

---
+++ CrackCodingInterview/LinkedLists/2.4partitionLinkedList.js
@@ -0,0 +1,8 @@
+//https://www.lintcode.com/problem/partition-list/description
+
+class LLNode {
+  constructor(value, next) {
+    this.value = value? value : 0
+    this.next = next? next : null
+  }
+}
