98e2d4302a6dce4d0c1a4df3b4276466f6ff23d9
dcnycoder <dc5stars@gmail.com>
2020-12-28 17:48:32 -0500

    8.1tripleStep: started

---
+++ CrackCodingInterview/RecursionDP/8.1tripleStep.js
@@ -0,0 +1,32 @@
+// A child is running up a staircase with n steps and can
+// hop either 1 step, 2 steps or 3 steps at a time, 
+// implement a method to count how many possible ways
+// the child can run up the stairs
+
+// You are climbing a staircase. It takes n steps to reach the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+// Example 1:
+
+// Input: n = 2
+// Output: 2
+// Explanation: There are two ways to climb to the top.
+// 1. 1 step + 1 step
+// 2. 2 steps
+// Example 2:
+
+// Input: n = 3
+// Output: 3
+// Explanation: There are three ways to climb to the top.
+// 1. 1 step + 1 step + 1 step
+// 2. 1 step + 2 steps
+// 3. 2 steps + 1 step
+
+const variations = [1,2,3]
+const steps = 3
+
+function tripleStep(steps, result = []) {
+
+}
+
