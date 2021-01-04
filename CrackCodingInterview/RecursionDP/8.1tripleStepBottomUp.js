e5a12e1d7a8fb130c6afea1127fdb3bc97ecf8dd
dcnycoder <dc5stars@gmail.com>
2021-01-04 06:25:05 -0500

    8.1tripleStepBottomUp.js started

---
+++ CrackCodingInterview/RecursionDP/8.1tripleStepBottomUp.js
@@ -0,0 +1,10 @@
+steps = 5
+variations = [1,2,3]
+
+
+function climbStairs(steps, result = []) {
+  for (let i=0; i<variations.length; i++) {
+    const variation = variations[i]
+    if (variation <= steps) return 
+  }
+}
