9ea2747ad958b55b809aaf263743ae4313419159
dcnycoder <dc5stars@gmail.com>
2021-01-09 18:16:28 -0500

    checkPalindrome: started

---
+++ CrackCodingInterview/ArraysStrings/checkPalindrome.js
@@ -0,0 +1,10 @@
+function checkPalindrome(str) {
+  let start = 0
+  let end = -1
+  result = true
+  while (start <= end && result!=false) {
+    if (str[start] === str[end])
+  }
+}
+
+const str = aba
