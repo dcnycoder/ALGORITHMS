3011ed32f9e3d2c04852f0baef525b527b5d137b
dcnycoder <dc5stars@gmail.com>
2021-01-23 11:11:55 -0500

    lengthOfLongestSubstring.js: started

---
+++ MS/STRINGS/lengthOfLongestSubstring.js
@@ -0,0 +1,12 @@
+// Given a string s, find the length of the longest substring without repeating characters.
+
+
+var lengthOfLongestSubstring = function(s) {
+    let charIndices = {}
+    let max = 0
+    s.forEach(char => console.log(char))
+    return max
+}
+
+const str = 'abcdafghb'
+console.log(lengthOfLongestSubstring(str))
