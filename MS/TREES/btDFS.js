ddc83f04c62bef93361a13fb01f0fb5447d0897b
dcnycoder <dc5stars@gmail.com>
2021-01-25 12:42:19 -0500

    btDFS: started

---
+++ MS/TREES/btDFS.js
@@ -0,0 +1,20 @@
+function BTBuilder(array) {
+  array[0] = new Node(array[0])
+  for (let i=0; i<array.length; i++) {
+    if (array[i]!=null) {
+      let currentNode = array[i]
+      if (array[2*i+1] && array[2*i+1] != null) {
+        array[2*i+1] = new Node(array[2*i+1])
+        currentNode.left = array[2*i+1]
+      }
+      else currentNode.left = null
+
+      if (array[2*i+2] && array[2*i+2] != null) {
+        array[2*i+2] = new Node(array[2*i+2])
+        currentNode.right = array[2*i+2]
+      }
+      else currentNode.right = null
+    }
+  }
+  return array[0]
+} // end of BTBuilder
