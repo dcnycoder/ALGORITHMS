5f0398bc5c3c492e1d3b56417829e38d95433267
dcnycoder <dc5stars@gmail.com>
2021-01-24 19:35:50 -0500

    anagrams.js: done

--- MS/STRINGS/anagrams.js
+++ MS/STRINGS/anagrams.js
@@ -1,10 +1,12 @@
-function anagrams(str1, str2) {
+function isAnagram(str1, str2) {
   if (str1.length != str2.length) return false
   let counter1 = {}
   let counter2 = {}
   for (let i=0; i<str1.length; i++) {
     if (!(str1[i] in counter1)) counter1[str1[i]] = 1
+    else counter1[str1[i]] += 1
     if (!(str2[i] in counter2)) counter2[str2[i]] = 1
+    else counter2[str2[i]] += 1
   }
   for (let letter in counter1) {
     if (!(letter in counter2) || (counter2[letter] != counter1[letter])) return false
@@ -14,4 +16,4 @@ function anagrams(str1, str2) {
 
 str1 = 'abcd'
 str2 = 'abce'
-console.log(anagrams(str1, str2))
+console.log(isAnagram(str1, str2))
