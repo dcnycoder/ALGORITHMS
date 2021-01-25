49827df17f0fc7e1f252f96186675206ba2339fc
dcnycoder <dc5stars@gmail.com>
2021-01-25 13:03:19 -0500

    btDFS: done

--- MS/TREES/btDFS.js
+++ MS/TREES/btDFS.js
@@ -1,3 +1,11 @@
+class Node {
+  constructor(val, left, right) {
+    this.val = (val === undefined)? 0 : val
+    this.left = (left === undefined)? null : left
+    this.right = (right === undefined)? null : right
+  }
+}
+
 function BTBuilder(array) {
   array[0] = new Node(array[0])
   for (let i=0; i<array.length; i++) {
@@ -18,3 +26,17 @@ function BTBuilder(array) {
   }
   return array[0]
 } // end of BTBuilder
+
+//in-order: L, C, R
+function btDFS(node) {
+  let result
+  if (node === null) return []
+  else {
+    result = btDFS(node.left).concat(node.val).concat(btDFS(node.right))
+  }
+  return result
+}
+
+const bt = BTBuilder([1,2,3,4,5,6,7,8])
+console.log("BT: ", bt)
+console.log(btDFS(bt))
