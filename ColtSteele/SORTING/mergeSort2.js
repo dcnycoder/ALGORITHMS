7010bfec3eb7501d0909b30b83b7411f4fea3df9
dcnycoder <dc5stars@gmail.com>
2021-03-11 13:19:58 -0500

    mergeSort2: done but needs optimization

--- ColtSteele/SORTING/mergeSort2.js
+++ ColtSteele/SORTING/mergeSort2.js
@@ -1,5 +1,5 @@
 function mergeSort(arr, start=0, end=arr.length) {
-  if (arr.length = 1) return arr
+  if (arr.length === 1) return arr
   else {
     let middle = start + Math.floor((end-start)/2)
     const left = arr.slice(start, middle);
@@ -8,9 +8,29 @@ function mergeSort(arr, start=0, end=arr.length) {
   }
 
   function merge(left, right) {
-    
+    let result = []
+    left = mergeSort(left);
+    right = mergeSort(right);
+
+    let leftIndex = 0
+    let rightIndex = 0
+    while (leftIndex<left.length && rightIndex<right.length) {
+      if (left[leftIndex] <= right[rightIndex]) {
+        result.push(left[leftIndex])
+        leftIndex++
+      }
+      else {
+        result.push(right[rightIndex])
+        rightIndex++
+      }
+    }
+    result = result.concat(left.slice(leftIndex, left.length));
+    result = result.concat(right.slice(rightIndex, right.length))
+
+    return result;
   }
 }
 
 
 let arr = [1,5,8,10,2]
+console.log(mergeSort(arr))
