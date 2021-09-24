9fe6acf6a02e2ae121a2dd283dd4c4c94c2cb8ab
dcnycoder <dc5stars@gmail.com>
2021-09-23 20:41:10 -0400

    linkedListCycle: done

--- Leetcode/LISTS/linkedListCycle.js
+++ Leetcode/LISTS/linkedListCycle.js
@@ -1,8 +1,34 @@
-class LinkedList {
+class Node {
   constructor(val, next) {
     this.val = val? val : 0
     this.next = next? next : null
   }
 }
 
-const LL = new LinkedList
+const LL = new Node(1)
+const Node2 = new Node(2)
+
+LL.next = Node2
+Node2.next = new Node(3)
+Node2.next.next = Node2
+
+var hasCycle = function(head) {
+  let nodeObject = {}
+  let current = head
+  while (current != null) {
+    if (!(current.val in nodeObject)) nodeObject[current.val] = [current]
+    else {
+      //for (let i=0; i<nodeObject[current].length; i++) {
+      for (let i=0; i<nodeObject[current.val].length-1; i++) {
+      //for (let node in nodeObject[current.val]) {
+        const node = nodeObject[current.val][i]
+        if (node === current) return true
+      }
+      nodeObject[current.val].push(current)
+    }
+    current = current.next
+  }
+  return false
+}
+
+console.log(linkedListCycle(LL))
