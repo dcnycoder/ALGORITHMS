dfacdfba5ba047bca5743c7594f59f558476951c
dcnycoder <dc5stars@gmail.com>
2021-10-01 18:11:45 -0400

    longestSubstring.js done but not tested

--- Leetcode/60_Problems/longestSubstring.js
+++ Leetcode/60_Problems/longestSubstring.js
@@ -1,22 +1,28 @@
-let str = 'abcabc'
+let str = 'abcabcda'
 
 function longesetSubstring(str) {
   let max = 0
+  counter = 0
   let start = 0
   let chars = {}
 
   for(let i=0; i<str.length; i++) {
-    if (!(chars.contains(str[i]))) {
+    if (!(str[i] in chars)) {
       chars[str[i]] = i
-      //max+=1
+      max+=1
     }
     else {
       if (i>start) {
-        if ((i-start>max)) max = i-start
-        start = chars[chars[i]]
+        if ((counter>max)) {
+          max = counter
+          
+        }
+        counter = i-chars[str[i]]
+        start = chars[str[i]]
       }
     }
   }
-
   return max
 }
+
+console.log(longesetSubstring(str))
