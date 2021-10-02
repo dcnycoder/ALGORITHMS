94a86d2161600c0d99db091778fae2481e84e61d
DC <dc@DCs-MacBook-Pro.local>
2021-10-02 16:40:25 -0400

    longestSubstring passed preliminary test

--- Leetcode/60_Problems/longestSubstring.js
+++ Leetcode/60_Problems/longestSubstring.js
@@ -1,28 +1,30 @@
-let str = 'abcabcda'
+let str = "abcabcaa"
 
-function longesetSubstring(str) {
+function longestSubstring(str) {
   let max = 0
   counter = 0
   let start = 0
   let chars = {}
 
-  for(let i=0; i<str.length; i++) {
+  for (let i=0; i<str.length; i++) {
     if (!(str[i] in chars)) {
       chars[str[i]] = i
-      max+=1
+      counter+=1
     }
     else {
-      if (i>start) {
+      if (i>=start) {
         if ((counter>max)) {
           max = counter
           
         }
-        counter = i-chars[str[i]]
-        start = chars[str[i]]
+        counter = i-start//chars[str[i]]
+        start = chars[str[i]]+1
+        chars[str[i]] = i
       }
     }
   }
+  if (counter>max) max = counter
   return max
 }
 
-console.log(longesetSubstring(str))
+console.log(longestSubstring(str))
