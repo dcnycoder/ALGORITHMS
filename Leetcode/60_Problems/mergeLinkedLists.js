0f8a6cf8a8996bb5af20225c893fdc2d1fac3e54
DC <dc@DCs-MacBook-Pro.local>
2021-10-04 17:18:08 -0400

    mergeLinkedLists.js done/tested

--- Leetcode/60_Problems/mergeLinkedLists.js
+++ Leetcode/60_Problems/mergeLinkedLists.js
@@ -4,3 +4,38 @@ class Node {
         this.next = (next)? new Node(next) : null
     }
 }
+ 
+let LL1 = new Node(1)
+LL1.next = new Node(2)
+LL1.next.next = new Node(3)
+LL1.next.next.next = new Node(5)
+
+let LL2 = new Node(2)
+LL2.next = new Node(4)
+LL2.next.next = new Node(6)
+
+console.log("LL1: ", LL1)
+console.log("LL2: ", LL2)
+
+function mergeLinkedLists(ll1, ll2) {
+    let result = new Node()
+    let curr = result
+
+    while (ll1 && ll2) {
+        if (ll2.val < ll1.val) {
+            curr.next = ll2
+            ll2 = ll2.next
+        }
+        else {
+            curr.next = ll1
+            ll1 = ll1.next
+        }
+        curr = curr.next
+    }
+    if (!ll2) curr.next = ll1
+    else curr.next = ll2
+    
+    return result.next
+}
+
+console.log("result: ", mergeLinkedLists(LL1, LL2))
