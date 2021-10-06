b7563446183bfe331cced356f102a7252e83879d
DC <dc@DCs-MacBook-Pro.local>
2021-10-06 16:35:25 -0400

    removeDuplicatesSortedLL.js: done, not tested

--- Leetcode/60_Problems/removeDuplicatesSortedLL.js
+++ Leetcode/60_Problems/removeDuplicatesSortedLL.js
@@ -6,3 +6,28 @@ class Node {
         this.next = (next)? next : null
      }
 }
+
+let LL = new Node(1)
+LL.next = new Node(1)
+LL.next.next = new Node(2)
+LL.next.next.next = new Node(2)
+LL.next.next.next.next = new Node(2)
+LL.next.next.next.next.next = new Node(3)
+LL.next.next.next.next.next.next = new Node(5)
+
+const removeDuplicatesFromSortedLL = (head) => {
+    let slowPointer = head
+    let fastPointer
+    while (slowPointer) {
+        //while (slowPointer.next) {
+            fastPointer = slowPointer.next
+            while (fastPointer && (fastPointer.val === slowPointer.val)) fastPointer = fastPointer.next//slowPointer.next = fastPointer 
+        //}
+        slowPointer.next = fastPointer
+        slowPointer = slowPointer.next
+    }
+    return head
+}
+
+
+console.log(removeDuplicatesFromSortedLL(LL))
