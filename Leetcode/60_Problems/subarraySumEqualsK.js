af001fc3fd75da2c5c6cf5f888d2bc72188c9bc2
DC <dc5stars@gmail.com>
2021-11-04 12:30:44 -0400

    subarraySumEqualsK.js solution drafted not tested

--- Leetcode/60_Problems/subarraySumEqualsK.js
+++ Leetcode/60_Problems/subarraySumEqualsK.js
@@ -11,14 +11,34 @@
 // Input: nums = [1,2,3], k = 3
 // Output: 2
 
-
+const nums = [1,2,3]
+const k = 3
 var subarraySum = function(nums, k) {
     let slowPointer = 0
     let fastPointer = 0
+    let sum = nums[slowPointer]
     let counter = 0
     
+    while (slowPointer < nums.length && fastPointer < nums.length) {
+      if (sum === k) {
+        counter += 1
+        sum = sum - nums[slowPointer]// + nums[fastPointer]
+        slowPointer += 1
+        //fastPointer += 1
+      }
+      else if (sum < k) {
+        fastPointer += 1
+        sum += nums[fastPointer]
+        
+      }
+      else {
+        sum -= nums[slowPointer]
+        slowPointer += 1
+      }
 
+    }
 
-    
     return counter
 };
+
+console.log(subarraySum(nums,k))
