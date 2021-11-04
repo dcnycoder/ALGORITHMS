170fe49c094bdf62bd2b847cddfb58b8ac00658b
DC <dc5stars@gmail.com>
2021-11-04 12:08:18 -0400

    subarraySumEqualsK started

---
+++ Leetcode/60_Problems/subarraySumEqualsK.js
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/subarray-sum-equals-k/
+
+//Given an array of integers nums and an integer k, return the total number of continuous subarrays whose sum equals to k.
+
+// Example 1:
+
+// Input: nums = [1,1,1], k = 2
+// Output: 2
+// Example 2:
+
+// Input: nums = [1,2,3], k = 3
+// Output: 2
+
+
+var subarraySum = function(nums, k) {
+    let slowPointer = 0
+    let fastPointer = 0
+    let counter = 0
+    
+
+
+    
+    return counter
+};
