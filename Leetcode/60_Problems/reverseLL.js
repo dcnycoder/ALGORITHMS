10acc880251b6cd7ed410f4a2e277aafee3e96b5
DC <dc5stars@gmail.com>
2021-12-03 14:24:29 -0500

    reverseLL: done & tested

--- Leetcode/60_Problems/reverseLL.js
+++ Leetcode/60_Problems/reverseLL.js
@@ -9,11 +9,19 @@ function reverseLL(head) {
   let currentNode = head
   let oldPrev = null
 
-  while (currentNode.val) {
+  while (currentNode) {
     let oldNext = currentNode.next
     currentNode.next = oldPrev
     oldPrev = currentNode
+    //if (oldNext === null) return currentNode
+    //else 
     currentNode = oldNext
   }
+  return currentNode
 }
 
+const LL = new Node(1)
+LL.next = new Node(2)
+LL.next.next = new Node(3)
+console.log("LL: ", LL)
+console.log("Reversed LL: ", reverseLL(LL))
